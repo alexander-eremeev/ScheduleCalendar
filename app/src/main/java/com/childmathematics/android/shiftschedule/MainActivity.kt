8ea154e52dd1637f3eec6fad73ce0c027fd947eb
Jobs <alexander.v.eremeev@gmail.com>
2024-09-20 19:49:48 +0300

    update App

--- app/src/main/java/com/childmathematics/android/shiftschedule/MainActivity.kt
+++ app/src/main/java/com/childmathematics/android/shiftschedule/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.childmathematics.android.shiftschedule
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import com.childmathematics.android.basement.lib.in_app_update.ScheduleAppUpdateManager
 import com.childmathematics.android.shiftschedule.ui.navigation.drawer.DrawerApp
-import com.google.android.play.core.install.model.AppUpdateType
 
 class MainActivity : ComponentActivity() {
-    private lateinit var activityResultLauncher: ActivityResultLauncher<IntentSenderRequest>
-
-//    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    val scheduleAppUpdateManager = ScheduleAppUpdateManager(this)
-
 
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,57 +19,12 @@ class MainActivity : ComponentActivity() {
         // ==============================================
         super.onCreate(savedInstanceState)
         // ------------------------------------------------------------------
-        // an activity result launcher registered via registerForActivityResult
-        // средство запуска результата активности, зарегистрированное через registerForActivityResult
-        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
-                result ->
-            // Handle the result of the update flow here
-        }
 //-------------------------------------------------------------------------
 //        val appContainer = (application as MainApplication).container
         setContent {
             val widthSizeClass = calculateWindowSizeClass(this).widthSizeClass
-//            checkForUpdate()
             DrawerApp( widthSizeClass)
 //            RootHost(widthSizeClass) //
         }
     }
-    //-----------------------------------------------
-    /**
-     *    Types of AppUpdate:
-     *     -> AppUpdateType.IMMEDIATE
-     *     -> AppUpdateType.FLEXIBLE
-     */
-
-    private fun checkForUpdate() {
-        scheduleAppUpdateManager.setUpdateType(AppUpdateType.FLEXIBLE)
-        scheduleAppUpdateManager.checkForUpdate { isAvailable, _ ->
-//        scheduleAppUpdateManager.checkForUpdate { isAvailable, message ->
-//            binding.tvText.text = message
-            if (isAvailable) {
-            //    requestForUpdate()
-            }
-        }
-    }
-
-    private fun requestForUpdate() {
-        scheduleAppUpdateManager.requestForUpdate { isUpdated, _ ->
- //           binding.tvText.text = message
-            if (isUpdated) {
-                // Proceed with Code...
-                Log.d("TAG", "requestForUpdate: Running App...")
-            }
-        }
-    }
-
-    // Checks that the update is not stalled during 'onResume()'
-    override fun onResume() {
-        super.onResume()
-        scheduleAppUpdateManager.checkIfUpdateInstalled()
-    }
-
-    private fun showToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
-    //===============================================
-
 }
--- app/src/main/java/com/childmathematics/android/shiftschedule/ui/in_app_update/InAppUpdatePageScreen.kt
+++ app/src/main/java/com/childmathematics/android/shiftschedule/ui/in_app_update/InAppUpdatePageScreen.kt
@@ -1,12 +1,18 @@
 package com.childmathematics.android.shiftschedule.ui.in_app_update
 
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
 import android.app.ProgressDialog.show
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BackHand
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,15 +24,26 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.ProgressBarRangeInfo
+import androidx.compose.ui.semantics.SemanticsProperties.ProgressBarRangeInfo
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import com.childmathematics.android.basement.lib.in_app_update.InAppUpdateManager
 import com.childmathematics.android.basement.lib.in_app_update.InAppUpdateManager.UPDATEAVAILABLE
+import com.childmathematics.android.basement.lib.in_app_update.InAppUpdateManager.activityResultLauncher
 import com.childmathematics.android.basement.lib.in_app_update.InAppUpdateManager.appUpdateManager
 import com.childmathematics.android.shiftschedule.BuildConfig
 import com.childmathematics.android.shiftschedule.R
 import com.childmathematics.android.shiftschedule.theme.ScheduleCalendarTheme
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,10 +51,8 @@ internal fun InAppUpdatePageScreen(
     modifier: Modifier = Modifier,
     onBackClick: () -> Unit,
     updateViewModel: UpdateViewModel,
-    ) {
-    val DAYS_FOR_FLEXIBLE_UPDATE:Int = 5
-
-
+    )
+{
     ScheduleCalendarTheme {
         val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
         Scaffold(
@@ -48,139 +63,114 @@ internal fun InAppUpdatePageScreen(
                     onBackClick, scrollBehavior,
                 )
             },
-            content ={ padding ->
+            content = { padding ->
                 Column(
                     modifier = Modifier
                         .padding(padding)
                 ) {
-/*
-InAppUpdateManager.initCheckForUpdates(LocalContext.current,
-    InAppUpdateManager.activityResultLauncher)
 
-if(UPDATE_AVAILABLE){
-}
+                    Button(
+                        onClick = {  },
+                    )
+                    {
+                        Text(stringResource(id = R.string.UpdatePage),
+                            fontWeight = FontWeight.Bold,
+                        )
+                     }
 
-*/
-/*
-val context = LocalContext.current
-val appUpdateManager = AppUpdateManagerFactory.create(LocalContext.current)
-// Returns an intent object that you use to check for an update.
-// Возвращает объект намерения, который вы используете для проверки наличия обновления.
-val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-// Checks that the platform will allow the specified type of update.
-// Проверяет, разрешит ли платформа указанный тип обновления.
+                    var appInstalled: Boolean = false
 
-appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-        // This example applies an immediate update. To apply a flexible update
-        // instead, pass in AppUpdateType.FLEXIBLE
-        // Этот пример применяет немедленное обновление. Чтобы применить гибкое обновление
-        // вместо этого передайте AppUpdateType.FLEXIBLE
-            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
-        ) {
-            // Request the update.
-            // Запросить обновление.
-        }
-    }
+                    val appUpdateManager = AppUpdateManagerFactory.create(LocalContext.current)
+                    // Returns an intent object that you use to check for an update.
+                    // Возвращает объект намерения, который вы используете для проверки наличия обновления.
+                    val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+                    // Checks that the platform will allow the specified type of update.
+                    // Проверяет, разрешит ли платформа указанный тип обновления.
+                    /*
+                    val listener:InstallStateUpdatedListener
+                    appUpdateManager.registerListener(listener)
+                    */
+ //                   lateinit var launcher: ActivityResultLauncher<IntentSenderRequest>
 
- */
 
-    // Checks whether the platform allows the specified type of update,
-    // and current version staleness.
-    // Проверяет, допускает ли платформа указанный тип обновления,
-    // и актуальность текущей версии.
-if (BuildConfig.DEBUG) {
-    Log.d("InUpUpdatePageScreen", "Проверка, допускает ли платформа указанный тип обновления,\n" +
-            " и актуальность текущей версии.")
-}
-//--------------------------------------
- //===================================
-// Checks whether the platform allows the specified type of update,
-// and checks the update priority.
-// Проверяет, допускает ли платформа указанный тип обновления,
-// и проверяет приоритет обновления.
-/*
-appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-    if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-        && appUpdateInfo.updatePriority() >= 4 /* high priority */
-        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-        // Request an immediate update.
-        Toast.makeText(
-            context,
-            //                   stringResource(R.string.inUpUpdate_Request_update),
-            "Request an immediate update",
-            Toast.LENGTH_LONG
-        ).show()
-        appUpdateManager.startUpdateFlowForResult(
-            // Pass the intent that is returned by 'getAppUpdateInfo()'.
-            appUpdateInfo,
-            // an activity result launcher registered via registerForActivityResult
-            activityResultLauncher,
-            // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
-            // flexible updates.
-            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
-                .build())
+                    lateinit var activityResultLauncher: ActivityResultLauncher<IntentSenderRequest>
 
-    }
-    // гибкое обновление
-    else if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-            && (appUpdateInfo.clientVersionStalenessDays() ?: -1) >= DAYS_FOR_FLEXIBLE_UPDATE
-            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
-        Toast.makeText(
-            context,
-            //                   stringResource(R.string.inUpUpdate_Request_update),
-            "Request the update",
-            Toast.LENGTH_LONG
-        ).show()
-        // Request the update.
-        //---------------------------------------------
-        // Create a listener to track request state updates.
-        val listener = InstallStateUpdatedListener { state ->
-            // (Optional) Provide a download progress bar.
-            if (state.installStatus() == InstallStatus.DOWNLOADING) {
-                val bytesDownloaded = state.bytesDownloaded()
-                val totalBytesToDownload = state.totalBytesToDownload()
-            // Show update progress bar.
-            }
-            // Log state or install the update.
-        }
+                    appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+                        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                            // This example applies an immediate update. To apply a flexible update
+                            // instead, pass in AppUpdateType.FLEXIBLE
+                            // Этот пример применяет немедленное обновление. Чтобы применить гибкое обновление
+                            // вместо этого передайте AppUpdateType.FLEXIBLE
+                            && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+                        ) {
+                            // Request the update.
+                            // Запросить обновление.
+                            //---------------------------------------------
+                            // Create a listener to track request state updates.
+                            // Before starting an update, register a listener for updates.
 
-        //---------------------------------------------------------
-        // Before starting an update, register a listener for updates.
-        appUpdateManager.registerListener(listener)
-        // Start an update.
-        // When status updates are no longer needed, unregister the listener.
-        appUpdateManager.unregisterListener(listener)
-        //-----------------------------------------
-        appUpdateManager.startUpdateFlowForResult(
-            // Pass the intent that is returned by 'getAppUpdateInfo()'.
-            appUpdateInfo,
-            // an activity result launcher registered via registerForActivityResult
-            activityResultLauncher,
-            // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
-            // flexible updates.
-            AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE)
-                .build()
+                            val listener = InstallStateUpdatedListener { state ->
+                                // (Optional) Provide a download progress bar.
+                                if (state.installStatus() == InstallStatus.DOWNLOADING) {
+                                    appInstalled = false
+
+                                    val bytesDownloaded = state.bytesDownloaded()
+                                    val totalBytesToDownload = state.totalBytesToDownload()
+                                    // Show update progress bar.
+                                    /*
+                                    ProgressBarRangeInfo(
+                                        current= bytesDownloaded.toFloat(),
+                                        range = totalBytesToDownload.toFloat(),
+                                   // steps = 0
+                                    )
+                                    
+                                     */
+//                                        Text("")
+
+                                }
+                                if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                                    // After the update is downloaded, show a notification
+                                    // and request user confirmation to restart the app.
+                                    // После загрузки обновления покажите уведомление
+                                    // и запросите подтверждение пользователя на перезапуск приложения.
+                                    // +++++++
+                                }
+                                if (state.installStatus() == InstallStatus.INSTALLED) {
+                                    appInstalled = true
+                                }
+                                 //-----------------------------------------
+
+                            }
+
+
+                            appUpdateManager.registerListener(listener)
+
+                            appUpdateManager.startUpdateFlowForResult(
+                                // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                                appUpdateInfo,
+                                // an activity result launcher registered via registerForActivityResult
+                                activityResultLauncher,
+                                // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+                                // flexible updates.
+                                AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE)
+                                    .build()
+                            )
+                            //_____________________________________
+                            if (appInstalled)appUpdateManager.unregisterListener(listener)
+
+
+                            //_____________________________________
+
+                        }
+                    }
+                }
+            },
         )
-        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
-            result: Instrumentation.ActivityResult ->
-            // handle callback
-            if (result.resultCode != RESULT_OK) {
-                log("Update flow failed! Result code: " + result.resultCode);
-                // If the update is canceled or fails,
-                // you can request to start the update again.
-            }
-        }
-        //_____________________________________
     }
+}
 
-}
-*/
-}
-},
-)
-}
-}
+
+
 //=================================
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
